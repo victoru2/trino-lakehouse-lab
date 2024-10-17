63be9e7dffadaf048ed1c6958539ef7604bbadcd
vurquiola <victor.urquiola@muttdata.ai>
2024-10-17 16:03:15 -0300

    atualizando modelagem bronze, doc e ip externo

--- airflow/dags/dbt/trino/models/bronze/binancecoin_bronze.sql
+++ airflow/dags/dbt/trino/models/bronze/binancecoin_bronze.sql
@@ -6,13 +6,7 @@
 }}
 WITH raw_data AS (
 SELECT DISTINCT
-    'bnb' AS coin_id,
-    'binancecoin' AS coin_name,
-	_airbyte_raw_id AS id,
-    _airbyte_extracted_at AS extracted_at,
-    _airbyte_generation_id AS generation_id,
-    _airbyte_meta AS metadata,
-    _airbyte_data AS raw_data
+    raw_data
 FROM
 	{{ source('minio', 'binancecoin') }})
 SELECT
--- airflow/dags/dbt/trino/models/bronze/cardano_bronze.sql
+++ airflow/dags/dbt/trino/models/bronze/cardano_bronze.sql
@@ -6,13 +6,7 @@
 }}
 WITH raw_data AS (
 SELECT DISTINCT
-    'ada' AS coin_id,
-    'cardano' AS coin_name,
-	_airbyte_raw_id AS id,
-    _airbyte_extracted_at AS extracted_at,
-    _airbyte_generation_id AS generation_id,
-    _airbyte_meta AS metadata,
-    _airbyte_data AS raw_data
+    raw_data
 FROM
 	{{ source('minio', 'cardano') }})
 SELECT
